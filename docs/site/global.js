0878de6132fc3043905726a902ee30dc0abc463c
Nathan Birch <birchn@byui.edu>
2021-04-01 14:20:43 -0600

    copied from JavaScript1 course.

---
+++ docs/site/global.js
@@ -0,0 +1,46 @@
+// global constants
+var g = {};
+
+// course info
+g.courseCode = '';
+g.courseName = 'Intro to JavaScript';
+g.courseCodeName = g.courseCode + ' | ' + g.courseName;
+
+// lessons
+g.n01 = 'Lesson ' + '01';
+g.n02 = 'Lesson ' + '02';
+g.n03 = 'Lesson ' + '03';
+g.n04 = 'Lesson ' + '04';
+g.n05 = 'Lesson ' + '05';
+
+// module names
+g.u01 = g.n01 + ' - Introduction to JavaScript';
+g.u02 = g.n02 + ' - Data Structures';
+g.u03 = g.n03 + ' - Document Object Model';
+g.u04 = g.n04 + ' - Web Application Part I';
+g.u05 = g.n05 + ' - Web Application Part II';
+
+// assignment information
+g.reading = "Reading Materials";
+g.classTime = "In-class Materials";
+g.assignment = "Programming Assignment";
+
+// course topics
+g.t1_0 = "What is JavaScript?";
+g.t1_1 = "Values / Types / Operators";
+g.t1_2 = "Execution Environments";
+g.t1_3 = "Variables";
+g.t1_4 = "Expressions and Statements";
+g.t1_5 = "Syntax";
+g.t1_6 = "Control Flow";
+
+g.t2_1 = "Control Flow";
+g.t2_2 = "Data Structures";
+g.t2_3 = "Native Variable Types";
+g.t2_4 = "Looping/Repetition";
+g.t2_5 = "Functions";
+
+g.t3_1 = "The DOM (Document Object Model)";
+g.t3_2 = "Events";
+g.t3_3 = "Forms - Elements and Validation";
+
