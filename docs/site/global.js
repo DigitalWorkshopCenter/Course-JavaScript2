3fb1aac0762433666b96f9696cc83077c98786cc
Nathan Birch <birchn@byui.edu>
2021-04-22 14:40:16 -0600

    working on lesson 1, about to try to incorporate accordions to shorten up pages a bit

--- docs/site/global.js
+++ docs/site/global.js
@@ -3,7 +3,7 @@ var g = {};
 
 // course info
 g.courseCode = '';
-g.courseName = 'Intro to JavaScript';
+g.courseName = 'JavaScript Course | Level II';
 g.courseCodeName = g.courseCode + ' | ' + g.courseName;
 
 // lessons
@@ -14,11 +14,11 @@ g.n04 = 'Lesson ' + '04';
 g.n05 = 'Lesson ' + '05';
 
 // module names
-g.u01 = g.n01 + ' - Introduction to JavaScript';
-g.u02 = g.n02 + ' - Data Structures';
-g.u03 = g.n03 + ' - Document Object Model';
-g.u04 = g.n04 + ' - Web Application Part I';
-g.u05 = g.n05 + ' - Web Application Part II';
+g.u01 = g.n01 + ' - Diving Deeper Into JavaScript';
+g.u02 = g.n02 + ' - JavaScript Dependencies and Builds';
+g.u03 = g.n03 + ' - JavaScript Frameworks';
+g.u04 = g.n04 + ' - Testing and Offline Storage';
+g.u05 = g.n05 + ' - Asynchronous JavaScript';
 
 // assignment information
 g.reading = "Reading Materials";
@@ -26,21 +26,30 @@ g.classTime = "In-class Materials";
 g.assignment = "Programming Assignment";
 
 // course topics
-g.t1_0 = "What is JavaScript?";
-g.t1_1 = "Values / Types / Operators";
-g.t1_2 = "Execution Environments";
-g.t1_3 = "Variables";
-g.t1_4 = "Expressions and Statements";
-g.t1_5 = "Syntax";
-g.t1_6 = "Control Flow";
+g.t1_0 = "JavaScript Review";
+g.t1_1 = "JavaScript Best Practices - Good Habits to get into";
+g.t1_2 = "Helpful Programming Techniques and Tips";
+g.t1_3 = "JavaScript Modules";
+g.t1_4 = "JavaScript Callbacks";
+g.t1_5 = "Evolution of JavaScript";
 
-g.t2_1 = "Control Flow";
-g.t2_2 = "Data Structures";
-g.t2_3 = "Native Variable Types";
+g.t2_1 = "Dependencies in JavaScript";
+g.t2_2 = "Builds";
+g.t2_3 = "Variable Types";
 g.t2_4 = "Looping/Repetition";
 g.t2_5 = "Functions";
 
-g.t3_1 = "The DOM (Document Object Model)";
-g.t3_2 = "Events";
-g.t3_3 = "Forms - Elements and Validation";
+g.t3_1 = "JavaScript Frameworks";
+g.t3_2 = "Angular";
+g.t3_3 = "React";
+g.t3_4 = "Vue";
+g.t3_5 = "Ember";
+g.t3_6 = "Meteor";
+g.t3_7 = "Node";
+
+g.t4_1 = "JavaScript Testing";
+g.t4_2 = "Offline Storage";
+
+g.t5_1 = "Asynchronous JavaScript";
+
 
