589af6891d1731b80e395976b823a1e26b6c0ddd
Nathan Birch <birchn@byui.edu>
2021-05-11 13:30:56 -0600

    Lesson02 Reading Complete

--- docs/site/global.js
+++ docs/site/global.js
@@ -15,41 +15,41 @@ g.n05 = 'Lesson ' + '05';
 
 // module names
 g.u01 = g.n01 + ' - Diving Deeper Into JavaScript';
-g.u02 = g.n02 + ' - JavaScript Dependencies and Builds';
-g.u03 = g.n03 + ' - JavaScript Frameworks';
+g.u02 = g.n02 + ' - JavaScript Frameworks';
+g.u03 = g.n03 + ' - JavaScript Dependencies and Builds';
 g.u04 = g.n04 + ' - Testing and Offline Storage';
 g.u05 = g.n05 + ' - Asynchronous JavaScript';
 
 // assignment information
-g.reading = "Reading Materials";
-g.classTime = "In-class Materials";
-g.assignment = "Programming Assignment";
+
+g.reading = 'Reading Materials';
+g.classTime = 'In-class Materials';
+g.assignment = 'Programming Assignment';
 
 // course topics
-g.t1_0 = "JavaScript Review";
-g.t1_1 = "JavaScript Best Practices - Good Habits to get into";
-g.t1_2 = "Helpful Programming Tips";
-g.t1_3 = "JavaScript Modules";
-g.t1_4 = "JavaScript Callbacks";
-g.t1_5 = "Evolution of JavaScript";
+g.t1_0 = 'JavaScript Review';
+g.t1_1 = 'JavaScript Best Practices - Good Habits to get into';
+g.t1_2 = 'Helpful Programming Tips';
+g.t1_3 = 'JavaScript Modules';
+g.t1_4 = 'JavaScript Callbacks';
+g.t1_5 = 'Evolution of JavaScript';
 
-g.t2_1 = "Dependencies in JavaScript";
-g.t2_2 = "Builds";
-g.t2_3 = "Variable Types";
-g.t2_4 = "Looping/Repetition";
-g.t2_5 = "Functions";
+g.t2_1 = 'JavaScript Frameworks';
+g.t2_2 = 'Angular';
+g.t2_3 = 'React';
+g.t2_4 = 'Vue';
+g.t2_5 = 'Ember';
+g.t2_6 = 'Meteor';
+g.t2_7 = 'Node';
+g.t2_8 = 'Comparing and Deciding';
 
-g.t3_1 = "JavaScript Frameworks";
-g.t3_2 = "Angular";
-g.t3_3 = "React";
-g.t3_4 = "Vue";
-g.t3_5 = "Ember";
-g.t3_6 = "Meteor";
-g.t3_7 = "Node";
+g.t3_1 = 'Dependencies in JavaScript';
+g.t3_2 = 'Builds';
+g.t3_3 = 'Variable Types';
+g.t3_4 = 'Looping/Repetition';
+g.t3_5 = 'Functions';
 
-g.t4_1 = "JavaScript Testing";
-g.t4_2 = "Offline Storage";
+g.t4_1 = 'JavaScript Testing';
+g.t4_2 = 'Offline Storage';
 
-g.t5_1 = "Asynchronous JavaScript";
-
-
+g.t5_1 = 'Asynchronous JavaScript';
