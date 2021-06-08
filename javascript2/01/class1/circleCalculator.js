bc840bb77f75146341abb0b781f2be2a3de7a42c
Nathan Birch <birchn@byui.edu>
2021-06-08 07:07:57 -0600

    removing old files

--- javascript2/01/class1/circleCalculator.js
+++
@@ -1,5 +0,0 @@
-export function calculateCircumference(r) {
-    return Math.PI * r * 2;
-}
-
-// export {calculateCircumference};
--- javascript2/01/class2/circleCalculator.js
+++
@@ -1,8 +0,0 @@
-export function calculateCircumference(r) {
-    return Math.PI * r * 2;
-}
-
-export function calculateArea(r) {
-    return Math.PI * r * r;
-}
-// export {calculateCircumference};
